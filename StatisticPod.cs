b0980f082447e63adcdb9104b502985d7502704b
Michael Matus <michael@matus.at>
2021-03-30 20:47:43 +0200

    standard deviation implemented

--- StatisticPod.cs
+++ StatisticPod.cs
@@ -21,12 +21,13 @@ namespace At.Matus.StatisticPod
         public double MinimumValue { get; private set; }
         public double Range => MaximumValue - MinimumValue;
         public double CentralValue => (MaximumValue + MinimumValue) / 2.0;
-        public double Variance => 0;
+        public double Variance => (SampleSize > 1) ? squareSum / (SampleSize - 1) : double.NaN;
         public double StandardDeviation => Math.Sqrt(Variance);
 
         public void Restart()
         {
             SampleSize = 0;
+            squareSum = 0.0;
             AverageValue = double.NaN;
             MaximumValue = double.NaN;
             MinimumValue = double.NaN;
@@ -47,20 +48,25 @@ namespace At.Matus.StatisticPod
                 MinimumValue = value;
             }
             MostRecentValue = value;
-            // https://diego.assencio.com/?index=c34d06f4f4de2375658ed41f70177d59
             // https://www.johndcook.com/blog/standard_deviation/
+            double oldAverage = AverageValue;
             AverageValue += (value - AverageValue) / SampleSize;
+            squareSum += (value - oldAverage) * (value - AverageValue); // TODO only if (SampleSize != 1) ?
             if (value > MaximumValue) MaximumValue = value;
             if (value < MinimumValue) MinimumValue = value;
         }
 
-        //TODO modify for StandardDeviation
-        public override string ToString() => SampleSize > 0
-            ? $"{Name} : {AverageValue} ± {StandardDeviation}"
-            : $"{Name} : {noDataYet}";
+        public override string ToString()
+        {
+            if (SampleSize == 0) return $"{Name} : {noDataYet}";
+            if (SampleSize == 1) return $"{Name} : {AverageValue}";
+            return $"{Name} : {AverageValue} ± {StandardDeviation}";
+        }
 
         private const string noNameSpecified = "<name not specified>";
         private const string noDataYet = "no data yet";
+
+        private double squareSum;
 
     }
 
